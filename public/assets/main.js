2fd0322c805ffb1923711e6c83aee3c99a188aa9
JLimaRodrigues <jeffersonlima147@hotmail.com>
2023-12-24 18:10:14 -0300

    tornando o navbar dinâmico

--- public/assets/main.js
+++ public/assets/main.js
@@ -1,5 +1,21 @@
 $(document).ready(() => {
 
+    // Obtém a URL atual
+    let currentUrl = window.location.pathname;
+
+    // Itera sobre os links e verifica se a URL atual corresponde à URL do link
+    $('.nav-link').each(function() {
+        let linkUrl = $(this).attr('href');
+
+        // Condição especial para tratar a URL "/"
+        if (currentUrl === '/' && linkUrl === '/') {
+            $(this).addClass('active');
+        } else if (currentUrl.startsWith(linkUrl) && linkUrl !== '/') {
+            // Se a URL atual começa com a URL do link (exceto "/"), adiciona a classe "active"
+            $(this).addClass('active');
+        }
+    });
+
     //criar usuário
     $('#novo-usuario').on('click', e => {
         e.preventDefault();
