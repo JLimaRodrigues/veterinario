59ab4daa40e1181810dfa2383733f37f2065667b
JLimaRodrigues <jeffersonlima147@hotmail.com>
2024-06-04 22:25:50 -0300

    resolvendo problema do tsconfig.json

--- src/entities/product.entity.ts
+++ src/entities/product.entity.ts
@@ -1,4 +1,5 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm"
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from 'uuid';
 
 @Entity('products')
 export class Product {
@@ -16,4 +17,10 @@ export class Product {
         type: 'timestamp'
     })
     createdAt: Date;
+
+    constructor(){
+        if(!this.id){
+            this.id = uuid()
+        }
+    }
 }
